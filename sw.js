b54b950feac9554c9828952ebf6279c48db2a3e2
shentreb <gabinessheabert@gmail.com>
2026-07-01 02:40:27 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('safepaws-admin-v1').then(cache => cache.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
