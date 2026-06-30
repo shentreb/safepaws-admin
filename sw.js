25bd5d38002bb4b50174b2de238f1459983a170f
shentreb <gabinessheabert@gmail.com>
2026-07-01 03:32:54 +0800

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,24 +1,4 @@
-const CACHE_NAME = 'safepaws-admin-v2.2';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-// Install Service Worker and cache assets
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-});
-
-// Serve cached content when offline
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+const CACHE_NAME = 'safepaws-admin-v2.4';
+const ASSETS_TO_CACHE = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', (event) => {event.waitUntil(caches.open(CACHE_NAME).then((cache) => {return cache.addAll(ASSETS_TO_CACHE);});
+self.addEventListener('fetch', (event) => {event.respondWith(caches.match(event.request).then((response) => {return response || fetch(event.request);});
